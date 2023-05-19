ba9997a8244bc87053a12ffd70d6ab6ad149d01c
Jaycie Ewald <jaycie.ewald@outlook.com>
2023-05-18 22:34:55 -0500

    fix up cmakelists, add shim

--- source/main.cpp
+++ source/main.cpp
@@ -11,19 +11,29 @@
 #include <stdint.h>
 #include <stdarg.h>
 
+// shim
+#include "shim.h"
+
+// gl
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/ostinygl.h>
+
 // kaitai
 #include "lev_quake.h"
 
 // tinyfiledialogs
-#include "tinyfiledialogs.h"
+#include "thirdparty/tinyfiledialogs.h"
 
-// imgui
-#include "imgui/imgui.h"
-#include "imgui/backends/imgui_impl_glfw.h"
-#include "imgui/backends/imgui_impl_opengl3.h"
+//
+//
+// globals
+//
+//
 
-// gl
-#include "GLFW/glfw3.h"
+#define WIDTH 800
+#define HEIGHT 600
+#define BPP 16
 
 //
 //
@@ -33,142 +43,16 @@
 
 const char *LevPatterns[2] = {"*.lev", "*.LEV"};
 const char *LevFilename = NULL;
-GLFWwindow *GlfwWindow = NULL;
 std::ifstream *FileStream = NULL;
 kaitai::kstream *KaitaiStream = NULL;
 lev_quake_t *Lev = NULL;
+ostgl_context *GlContext = NULL;
 
 //
 //
 // functions
 //
 //
-
-//
-// Error
-//
-
-void Error(const char *s, ...)
-{
-	va_list ap;
-	char buffer[128];
-
-	va_start(ap, s);
-	vsprintf(buffer, s, ap);
-	va_end(ap);
-
-	std::cerr << buffer << std::endl;
-
-	exit(1);
-}
-
-//
-// GlfwError
-//
-
-void GlfwError()
-{
-	const char *error;
-	glfwGetError(&error);
-	Error("%s", error);	
-}
-
-//
-// Init
-//
-
-void Init(int w, int h, const char *title)
-{
-	// init library
-	if (glfwInit() != GLFW_TRUE)
-		GlfwError();
-
-	// open window
-	glfwDefaultWindowHints();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	GlfwWindow = glfwCreateWindow(w, h, title, NULL, NULL);
-	if (GlfwWindow == NULL)
-		GlfwError();
-
-	// make current
-	glfwMakeContextCurrent(GlfwWindow);
-	glfwSwapInterval(1);
-
-	// init imgui
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO &io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplGlfw_InitForOpenGL(GlfwWindow, true);
-	ImGui_ImplOpenGL3_Init("#version 130");
-}
-
-//
-// DeInit
-//
-
-void DeInit()
-{
-	// imgui
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-
-	// close window
-	if (GlfwWindow)
-		glfwDestroyWindow(GlfwWindow);
-
-	// cloes library
-	glfwTerminate();
-
-	// clean up memory
-	if (Lev) delete Lev;
-	if (FileStream) delete FileStream;
-	if (KaitaiStream) delete KaitaiStream;
-}
-
-//
-// StartFrame
-//
-
-void StartFrame()
-{
-	// glfw
-	glfwPollEvents();
-
-	// imgui
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplGlfw_NewFrame();
-	ImGui::NewFrame();
-}
-
-//
-// EndFrame
-//
-
-void EndFrame()
-{
-	// clear
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// imgui
-	ImGui::Render();
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-	// show
-	glfwSwapBuffers(GlfwWindow);
-}
-
-//
-// QuitMainLoop
-//
-
-void QuitMainLoop()
-{
-	glfwSetWindowShouldClose(GlfwWindow, GLFW_TRUE);
-}
 
 //
 // LoadLev
@@ -189,12 +73,20 @@ void LoadLev(const char *filename)
 	else
 	{
 		LevFilename = tinyfd_openFileDialog("Choose a LEV File", "", 2, LevPatterns, "Quake LEV Files", 0);
-		if (LevFilename == NULL) Error("LevFilename is NULL!");
+		if (LevFilename == NULL)
+		{
+			shim_error("LevFilename is NULL!");
+			exit(1);
+		}
 	}
 
 	// filestream
 	FileStream = new std::ifstream(LevFilename, std::ifstream::binary);
-	if (!FileStream->is_open()) Error("Couldn't open %s!", LevFilename);
+	if (!FileStream->is_open())
+	{
+		shim_error("Couldn't open %s!", LevFilename);
+		exit(1);
+	}
 
 	// kaitaistream
 	KaitaiStream = new kaitai::kstream(FileStream);
@@ -209,40 +101,32 @@ void LoadLev(const char *filename)
 
 int main(int argc, char **argv)
 {
-	// load
+	// load lev
 	if (argc > 1)
 		LoadLev(argv[1]);
 	else
 		LoadLev(NULL);
 
-	// init
-	Init(800, 600, "lobo");
+	// init window
+	GlContext = ostgl_create_context(WIDTH, HEIGHT, BPP);
+	ostgl_make_current(GlContext);
+	shim_init(GlContext->width, GlContext->height, GlContext->depth, "lobo");
 
-	// main render loop
-	while (!glfwWindowShouldClose(GlfwWindow))
+	// main loop
+	while (shim_frame())
 	{
-		// poll events
-		StartFrame();
+		if (shim_key_read(SHIM_KEY_ESCAPE))
+			shim_should_quit(SHIM_TRUE);
 
-		// main menu bar
-		if (ImGui::BeginMainMenuBar())
-		{
-			if (ImGui::BeginMenu("File", true))
-			{
-				if (ImGui::MenuItem("Load")) { LoadLev(NULL); }
-				if (ImGui::MenuItem("Quit")) { QuitMainLoop(); }
-				ImGui::EndMenu();
-			}
+		glClearColor(1, 0, 1, 1);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-			ImGui::EndMainMenuBar();
-		}
-
-		// double buffer
-		EndFrame();
+		shim_blit(GlContext->width, GlContext->height, GlContext->depth, GlContext->pixels);
 	}
 
-	// deinit
-	DeInit();
+	// shutdown
+	ostgl_delete_context(GlContext);
+	shim_quit();
 
 	// return success
 	return 0;
